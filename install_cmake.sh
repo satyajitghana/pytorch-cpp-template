9b9db4b9133b320c9bc2058af297aca8ec683363
Satyajit Ghana <satyajitghana7@gmail.com>
2020-10-31 21:26:24 +0530

    add build files

---
+++ install_cmake.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# CMAKE latest
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+sudo apt update
+sudo apt install cmake -y
